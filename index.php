bcb7d107d194107cfd82e0c51b3adb65531fcca3
elitebug1-Hub <elitebug1@gmail.com>
2026-05-11 11:15:37 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$date = date('Y-m-d H:i:s');
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <title>My Web Server</title>
+</head>
+<body style="font-family:Arial; text-align:center; padding-top:80px;">
+  <h1>Hello World</h1>
+  <p>Cloudtype PHP Server is Running</p>
+  <p><?php echo $date; ?></p>
+</body>
+</html>
